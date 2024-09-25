3d11e25da4576535f3392546d63fe20f0e9c5c8c
dhwisema <dhwisema@ncsu.edu>
2024-09-25 15:16:30 -0400

    parity

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -109,6 +109,11 @@
   pkgs.git
   pkgs.lazygit
   pkgs.vscode
+  pkgs.usbmuxd
+  pkgs.ranger
+  pkgs.rclone
+  pkgs.rclone-browser
+  pkgs.keepassxc
 
 
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
