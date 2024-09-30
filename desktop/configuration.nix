7a2014c7b5d345121e3af6081a23a0c6a906ea2c
dhwisema <dhwisema@ncsu.edu>
2024-09-30 18:13:29 -0400

    guh

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -186,6 +186,8 @@
   pkgs.rclone-browser
   pkgs.lazygit
   pkgs.git
+  pkgs.xterm
+  pkgs.filezilla
 
   pkgs.libimobiledevice
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -118,6 +118,8 @@
   pkgs.libimobiledevice
   pkgs.boxbuddy
   pkgs.distrobox
+  pkgs.xterm
+  pkgs.filezilla
 
 
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
