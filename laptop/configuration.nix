7bdf07d5ab2b7624a2d1ce661d71a5f29dbd3081
dhwisema <dhwisema@ncsu.edu>
2024-10-01 05:21:42 -0400

    fix me being not smart

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -16,12 +16,6 @@
 
   hardware.bluetooth.enable=true;
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
