1ab705395c1619acf2f23d6ac68ae6ad0e7f1af0
dhwisema <dhwisema@ncsu.edu>
2024-10-13 04:51:23 -0400

    angy

--- Hosts/desktop/configuration.nix
+++ Hosts/desktop/configuration.nix
@@ -1,11 +1,22 @@
 { config, pkgs, lib, ... }: {
   imports = [
-      ./hardware-configuration.nix
+      #./hardware-configuration.nix
       ./../default.nix
-
   ];
   gaming.enable = true;
   nvidia.enable = true;
   networking.hostName = "deskbox";
   time.hardwareClockInLocalTime = true;
+
+
+    hardware.bluetooth.enable = true;
+
+    hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 }
