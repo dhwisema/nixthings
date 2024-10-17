70f024a7ca7c3e7c02614a7ce9ed8ec7e84b09ae
dhwisema <dhwisema@ncsu.edu>
2024-10-17 04:19:39 -0400

    i

--- flake.nix
+++ flake.nix
@@ -4,11 +4,12 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:nixos/nixos-hardware/master";
-
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
   };
 
-  outputs = { self, nixpkgs, nixos-hardware}:
+  outputs = { self, nixpkgs, nixos-hardware, home-manager}:
   
 {
     nixosConfigurations = {
@@ -17,8 +18,18 @@
         modules = [
           
           nixos-hardware.nixosModules.lenovo-thinkpad-z
+          home-manager.nixosModules.home-manager
           ./Hosts/laptop/configuration.nix
 
+
+                    {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.howard = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
         ];
       };
     };
@@ -29,9 +40,16 @@
         modules = [
           nixos-hardware.nixosModules.common-cpu-amd
           nixos-hardware.nixosModules.common-pc-ssd
-          
+          home-manager.nixosModules.home-manager
           ./Hosts/desktop/configuration.nix
-          
+                    {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.howard = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
           
         ];
       };
