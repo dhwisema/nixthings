16865bfa9f28a4ae8833c0a6744f31c6aedc8aff
dhwisema <dhwisema@ncsu.edu>
2024-10-17 06:01:07 -0400

    wer

--- flake.nix
+++ flake.nix
@@ -22,60 +22,56 @@
     home-manager,
     catppuccin,
   }: {
-    nixosConfigurations = {
-      laptop = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          nixos-hardware.nixosModules.lenovo-thinkpad-z
+    nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        nixos-hardware.nixosModules.lenovo-thinkpad-z
 
-          ./Hosts/laptop/configuration.nix
+        ./Hosts/laptop/configuration.nix
 
-          home-manager.nixosModules.home-manager
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
+        home-manager.nixosModules.home-manager
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
 
-            home-manager.users.howard = {
-              imports = [
-                ./Hosts/desktop/home.nix
-                catppuccin.homeManagerModules.catppuccin
-              ];
-            };
+          home-manager.users.howard = {
+            imports = [
+              ./Hosts/desktop/home.nix
+              catppuccin.homeManagerModules.catppuccin
+            ];
+          };
 
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-          }
-        ];
-      };
+          # Optionally, use home-manager.extraSpecialArgs to pass
+          # arguments to home.nix
+        }
+      ];
     };
 
-    nixosConfigurations = {
-      deskbox = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          nixos-hardware.nixosModules.common-cpu-amd
-          nixos-hardware.nixosModules.common-pc-ssd
+    nixosConfigurations.deskbox = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        nixos-hardware.nixosModules.common-cpu-amd
+        nixos-hardware.nixosModules.common-pc-ssd
 
-          ./Hosts/desktop/configuration.nix
+        ./Hosts/desktop/configuration.nix
 
-          home-manager.nixosModules.home-manager
+        home-manager.nixosModules.home-manager
 
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.howard = {
-              imports = [
-                ./Hosts/desktop/home.nix
-                catppuccin.homeManagerModules.catppuccin
-              ];
-            };
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.howard = {
+            imports = [
+              ./Hosts/desktop/home.nix
+              catppuccin.homeManagerModules.catppuccin
+            ];
+          };
 
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-          }
-        ];
-      };
+          # Optionally, use home-manager.extraSpecialArgs to pass
+          # arguments to home.nix
+        }
+      ];
     };
   };
 }
