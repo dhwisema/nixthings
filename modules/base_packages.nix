857432b5e5c1cc31b9f06f071b2f5e0a0202042a
dhwisema <dhwisema@ncsu.edu>
2024-10-17 04:52:38 -0400

    testing?

--- modules/base_packages.nix
+++ modules/base_packages.nix
@@ -1,4 +1,4 @@
-{ pkgs,... }: {
+{pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     gnome-software
     usbmuxd
@@ -6,20 +6,19 @@
     lazygit
     libreoffice-fresh
     libimobiledevice
-   
+    wezterm
   ];
   #enable and make fish defaultUserShell
   programs.fish.enable = true;
   users.defaultUserShell = pkgs.fish;
 
   #flatpak setup
-  services.flatpak.enable=true;
-    systemd.services.flatpak-repo = {
-    wantedBy = [ "multi-user.target" ];
-    path = [ pkgs.flatpak ];
+  services.flatpak.enable = true;
+  systemd.services.flatpak-repo = {
+    wantedBy = ["multi-user.target"];
+    path = [pkgs.flatpak];
     script = ''
       flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     '';
   };
-
 }
