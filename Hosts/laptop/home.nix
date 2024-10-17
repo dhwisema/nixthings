1753007eaacbc296108c9b3f10bb0e693477fb2c
dhwisema <dhwisema@ncsu.edu>
2024-10-17 04:37:32 -0400

    ....

--- Hosts/laptop/home.nix
+++ Hosts/laptop/home.nix
@@ -18,9 +18,12 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    pkgs.google-chrome-stable
+    pkgs.vscodium
+    pkgs.nixd
+    pkgs.vesktop
+    pkgs.alejandra
+
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
