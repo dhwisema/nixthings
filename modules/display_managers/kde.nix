708d59029059531d986933ca23f2878547863287
dhwisema <112828283+dhwisema@users.noreply.github.com>
2024-11-12 03:36:52 -0500

    Update kde.nix

--- modules/display_managers/kde.nix
+++ modules/display_managers/kde.nix
@@ -1,20 +1,19 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: {
+{ config, pkgs, lib, ... }: {
+
   options = {
     kde.enable = lib.mkOption {
       type = lib.types.bool;
       default = false;
-      description = "enable kde";
+      description = "enable kde.";
     };
   };
-  config = lib.mkIf config.gnome.enable {
-    services.xserver.enable = true;
-    services.displayManager.sddm.wayland.enable = true;
-    services.desktopManager.plasma6.enable = true;
-    services.displayManager.defaultSession = "plasma";
-  };
+
+
+    # Enable the KDE Plasma Desktop Environment.
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  services.displayManager.defaultSession="plasma";
+
 }
