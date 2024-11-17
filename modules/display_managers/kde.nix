37898dc35c6dfe1cb27944b1f736ff8a8c7f4ee2
dhwisema <dhwisema@ncsu.edu>
2024-11-17 03:44:14 -0500

    error

--- modules/display_managers/kde.nix
+++ modules/display_managers/kde.nix
@@ -1,5 +1,9 @@
-{ config, pkgs, lib, ... }: {
-
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
   options = {
     kde.enable = lib.mkOption {
       type = lib.types.bool;
@@ -8,12 +12,12 @@
     };
   };
 
-
+  config = lib.mkIf config.kde.enable {
     # Enable the KDE Plasma Desktop Environment.
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  services.displayManager.defaultSession="plasma";
-
+    # Enable the KDE Plasma Desktop Environment.
+    services.displayManager.sddm.enable = true;
+    services.displayManager.sddm.wayland.enable = true;
+    services.desktopManager.plasma6.enable = true;
+    services.displayManager.defaultSession = "plasma";
+  };
 }
