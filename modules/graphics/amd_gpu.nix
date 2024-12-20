fdaa7e3cfbc0c196c4a89317e5b3b2fdd3c6755d
dhwisema <dhwisema@ncsu.edu>
2024-12-19 23:03:50 -0500

    test

--- modules/graphics/amd_gpu.nix
+++ modules/graphics/amd_gpu.nix
@@ -5,4 +5,17 @@
     enable = true;
     enable32Bit = true;
   };
+
+
+  # Enable OpenGL
+hardware.opengl = {
+  enable = true;
+  driSupport = true;
+  driSupport32Bit = true;
+  extraPackages = with pkgs; [
+    vulkan-loader
+    vulkan-validation-layers
+    vulkan-extension-layer
+  ];
+};  
 }
