06f8a0134b0d9108e5a2503b4570356eb46387f3
dhwisema <dhwisema@ncsu.edu>
2024-12-19 23:22:12 -0500

    prevent silly and trying to fix laptop

--- Hosts/laptop/configuration.nix
+++ Hosts/laptop/configuration.nix
@@ -7,4 +7,13 @@
   gaming.enable = true;
   hardware.bluetooth.enable = true;
   networking.hostName = "laptop";
+
+
+  hardware.graphics.extraPackages = with pkgs; [
+  amdvlk
+  ];
+  # For 32 bit applications 
+  hardware.graphics.extraPackages32 = with pkgs; [
+  driversi686Linux.amdvlk
+  ];
 }
