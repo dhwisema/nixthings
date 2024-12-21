39193ba2a95f96ba2a5942447a48f47e0697ffb4
dhwisema <dhwisema@ncsu.edu>
2024-12-21 17:22:48 -0500

    package broke this sucks

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS Configurations for Desktop and Laptop";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs";
     #hardware
     nixos-hardware.url = "github:nixos/nixos-hardware/master";
     #flatpak
