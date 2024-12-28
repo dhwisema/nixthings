e8eb9f6e5a702eeca4de45706c360f2bbf8aa30a
dhwisema <dhwisema@ncsu.edu>
2024-12-28 01:16:29 -0500

    moving things and that

--- flake.nix
+++ flake.nix
@@ -52,7 +52,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.howard = import ./Hosts/home.nix;
+            home-manager.users.howard = import ./Hosts/laptop/home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
@@ -81,7 +81,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.howard = import ./Hosts/home.nix;
+            home-manager.users.howard = import ./Hosts/desktop/home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
