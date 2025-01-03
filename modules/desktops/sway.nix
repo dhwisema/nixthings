c6fe10a292309d80ed6e9ff8adf07f1aa4f7ae9e
dhwisema <dhwisema@ncsu.edu>
2025-01-03 17:06:26 -0500

    sway tings

--- modules/desktops/sway.nix
+++ modules/desktops/sway.nix
@@ -1,30 +1,45 @@
-{pkgs,...}:{
-  environment.systemPackages = with pkgs; [
-    brightnessctl
-    wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
-    mako # notification system developed by swaywm maintainer
-    grimshot # screenshot functions
-    sww #wallpapers and shi
-    wl-clipboard-history #clipboard history
-    fuzzel #launcher
-    waybar # bar
-    kanshi #displau
-    rofi-wayland # launcher as well???abort
-    wl sunset # white temp control
-    imv #image view
-  ];
-
-  # Enable the gnome-keyring secrets vault. 
-  # Will be exposed through DBus to programs willing to store secrets.
-  services.gnome.gnome-keyring.enable = true;
-
-  # enable sway window manager
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true;
-
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    sway.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "enable sway.";
+    };
   };
 
-  #enable light for backligh control
-  programs.light.enable = true;
+  config = lib.mkIf config.sway.enable {
+    environment.systemPackages = with pkgs; [
+      brightnessctl
+      wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
+      mako # notification system developed by swaywm maintainer
+      grimshot # screenshot functions
+      sww #wallpapers and shi
+      wl-clipboard-history #clipboard history
+      fuzzel #launcher
+      waybar # bar
+      kanshi #displau
+      rofi-wayland # launcher as well???abort
+      wl
+      sunset # white temp control
+      imv #image view
+    ];
+
+    # Enable the gnome-keyring secrets vault.
+    # Will be exposed through DBus to programs willing to store secrets.
+    services.gnome.gnome-keyring.enable = true;
+
+    # enable sway window manager
+    programs.sway = {
+      enable = true;
+      wrapperFeatures.gtk = true;
+    };
+
+    #enable light for backligh control
+    programs.light.enable = true;
+  };
 }
