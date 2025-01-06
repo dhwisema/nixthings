a3c12196fefcd9a37ec1a3e71e2630294a5a5314
dhwisema <dhwisema@ncsu.edu>
2025-01-05 23:53:17 -0500

    floorp

--- modules/packages/flatpak.nix
+++ modules/packages/flatpak.nix
@@ -14,7 +14,7 @@
     "com.calibre_ebook.calibre"
     "com.github.corna.Vivado"
     "io.github.flattool.Warehouse"
-    "io.github.zen_browser.zen"
+    "one.ablaze.floorp"
     "com.google.Chrome"
     "camp.nook.nookdesktop"
     "io.github.equicord.equibop"
