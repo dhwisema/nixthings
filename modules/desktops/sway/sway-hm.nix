793e094d29faa31122eafb69829d46ea0c031522
dhwisema <dhwisema@ncsu.edu>
2025-01-06 11:18:23 -0500

    sway

--- modules/desktops/sway/sway-hm.nix
+++ modules/desktops/sway/sway-hm.nix
@@ -1,6 +1,23 @@
-{pkgs,
+{
+  pkgs,
   lib,
   config,
-  ...}:{
+  ...
+}: {
+  wayland.windowManager.sway = {
+    extraOptions = "--unsupported-gpu";
+    enable = true;
+    wrapperFeatures.gtk = true; # Fixes common issues with GTK 3 apps
+    config = rec {
+      modifier = "Mod4";
+      # Use kitty as default terminal
+      terminal = "kitty";
+      startup = [
+        # Launch mako on start
+        {command = "mako";}
+        {command = "fuzzel";}
 
+      ];
+    };
+  }; #end config
 }
