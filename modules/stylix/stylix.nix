bc0a40c4d034b25ef33fb1135a3bea0f6c922228
dhwisema <dhwisema@ncsu.edu>
2025-01-12 04:07:40 -0500

    erm this shit broken

--- modules/stylix/stylix.nix
+++ modules/stylix/stylix.nix
@@ -5,7 +5,7 @@
   ...
 }: {
   stylix.enable = true;
-  stylix.image = "./wallpapers/tokyo_night/03.png";
+  stylix.image = ./wallpapers/tokyo_night/03.png;
   stylix.polarity = "dark";
   stylix.targets.kde.enable = true;
   stylix.autoEnable = true;
