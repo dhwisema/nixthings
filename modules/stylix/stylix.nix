837584318e134aa76c02a805241193db53f48ccd
dhwisema <dhwisema@ncsu.edu>
2025-01-12 04:14:52 -0500

    wallpaperfighting

--- modules/stylix/stylix.nix
+++ modules/stylix/stylix.nix
@@ -2,11 +2,14 @@
   config,
   lib,
   stylix,
+  pkgs,
   ...
 }: {
   stylix.enable = true;
-  stylix.image = "./wallpapers/tokyo_night/03.png";
+  stylix.image = pkgs.fetchurl {
+    url = "https://github.com/NotAShelf/wallpkgs/blob/main/wallpapers/cities/14.jpg";
+    sha256 = "940d83575aab170fa96b1f468f71cc8a4ac3ffdf4ccf0917dea13088c09b9cc6";
+  };
   stylix.polarity = "dark";
-  stylix.targets.kde.enable = true;
   stylix.autoEnable = true;
 }
