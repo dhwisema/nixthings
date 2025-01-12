6267caf8b9b10033caff79e380275ffcde386f81
dhwisema <dhwisema@ncsu.edu>
2025-01-12 04:22:17 -0500

    screaming crying hrowing up

--- modules/stylix/stylix.nix
+++ modules/stylix/stylix.nix
@@ -6,10 +6,7 @@
   ...
 }: {
   stylix.enable = true;
-  stylix.image = pkgs.fetchurl {
-    url = "https://github.com/NotAShelf/wallpkgs/blob/main/wallpapers/cities/14.jpg";
-    sha256 = "940d83575aab170fa96b1f468f71cc8a4ac3ffdf4ccf0917dea13088c09b9cc6";
-  };
+  stylix.image = ./wallpapers/tokyo_night/03.png;
   stylix.polarity = "dark";
   stylix.autoEnable = true;
 }
