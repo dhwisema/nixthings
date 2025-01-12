d6f5120e7511d575a1f8b6b38782ba5aea3f3b94
dhwisema <dhwisema@ncsu.edu>
2025-01-12 02:10:20 -0500

    a

--- modules/desktops/sway/sway-hm.nix
+++ modules/desktops/sway/sway-hm.nix
@@ -18,7 +18,7 @@
     enable = true;
     wrapperFeatures.gtk = true; # Fixes common issues with GTK 3 apps
     config = rec {
-      keybindings = lib./mkOptionDefault {
+      keybindings = lib.mkOptionDefault {
         #can pass multiple options via ''
         "${modifier}+d" = "exec ${pkgs.fuzzel}/bin/fuzzel";
         "${modifier}+b" = "flatpak run com.google.Chrome '--ozone-platform-hint=auto'";
