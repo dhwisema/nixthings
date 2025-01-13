22bfb5a2ce0d074b193865e9a336f430f342d42b
dhwisema <dhwisema@ncsu.edu>
2025-01-13 10:24:57 -0500

    silly goofy kernel things

--- modules/common/nix-nh.nix
+++ modules/common/nix-nh.nix
@@ -21,7 +21,7 @@
     settings = {
       substituters = [
         "https://cache.nixos.org/"
-        "https://cache.garnix.io"
+        #"https://cache.garnix.io"
         "https://nix-community.cachix.org"
         "https://nixpkgs-wayland.cachix.org"
       ];
@@ -29,7 +29,7 @@
         "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+       # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
 
       auto-optimise-store = true; # Optimise syslinks
