cfe0e6512cfb97cc899370f99f090441d0adf7e6
dhwisema <dhwisema@ncsu.edu>
2025-01-17 22:36:25 -0500

    +

--- modules/desktops/niri/niri-hm.nix
+++ modules/desktops/niri/niri-hm.nix
@@ -5,11 +5,6 @@
   ...
 }: {
 
-  programs.wpaperd.settings = {
-    path = "./nixthings/stylix/wallpapers/tokyo_night/03.png";
-  };
-
-
   programs.niri.settings.spawn-at-startup = [
     {command = ["waybar"];}
     {command = ["mako"];}
