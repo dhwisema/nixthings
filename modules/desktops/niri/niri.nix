52bd3269cc1345274dc9f18e14f61a508868353d
dhwisema <dhwisema@ncsu.edu>
2025-01-19 17:43:30 -0500

    back to sdd, qt5

--- modules/desktops/niri/niri.nix
+++ modules/desktops/niri/niri.nix
@@ -27,23 +27,18 @@
       swaylock
       sway-contrib.grimshot
       xwayland-satellite
-      xfce.xfce4-panel
       pavucontrol
       waypaper
       swww
       adwaita-icon-theme-legacy
       adwaita-icon-theme
       #sddm specific files
-      catppuccin-sddm-corners
-      libsForQt5.qt5.qtgraphicaleffects
-      libsForQt5.qt5.qtsvg
-      libsForQt5.qt5.qtquickcontrols
     ];
     programs.niri.enable = true;
 
-    services.displayManager.sddm = {
-      package = pkgs.kdePackages.sddm; # qt6 sddm version
-      extraPackages = with pkgs;[sddm-astronaut];
+      services.displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
     };
   };
 }
