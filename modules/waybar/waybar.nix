ab2737213df60579ed1a81a776aefc6997701edb
dhwisema <dhwisema@ncsu.edu>
2025-01-19 20:23:23 -0500

    edits

--- modules/waybar/waybar.nix
+++ modules/waybar/waybar.nix
@@ -8,11 +8,10 @@
     mainBar = {
       layer = "top";
       position = "top";
-      height = 30;
 
       modules-center = ["clock"];
-      modules-right = ["battery"];
-      modules-left = ["niri/workspaces" "niri-window"];
+      modules-right = ["battery" "bluetooth" "memory" "network" "pulseaudio"];
+      modules-left = ["niri/workspaces" "wlr/taskbar"];
     };
   };
 }
