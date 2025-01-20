1a3674a0b65bd1d9986f8a37d20d6491ac33b631
dhwisema <dhwisema@ncsu.edu>
2025-01-19 19:29:27 -0500

    ????

--- modules/desktops/niri/niri-hm.nix
+++ modules/desktops/niri/niri-hm.nix
@@ -6,15 +6,14 @@
 }: {
   stylix.targets.niri.enable = true;
 
-  programs.niri.settings.outputs."LG Electronics LG ULTRAGEAR 403MXEZ81564" = {
+  programs.niri.settings.outputs."DP-4" = {
     position.x = 0;
     position.y = 0;
     variable-refresh-rate = true;
   };
-  programs.niri.settings.outputs."Lenovo Group Limited T24i-2L VKMR655" = {
+  programs.niri.settings.outputs."DP-5" = {
     position.x = -1080;
     position.y = 0;
-    enable = false;
   };
 
   programs.niri.settings.spawn-at-startup = [
