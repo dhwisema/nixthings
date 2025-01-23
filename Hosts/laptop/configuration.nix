7a270879006cfde64787ff2eca6e99c1652ae2e0
dhwisema <dhwisema@ncsu.edu>
2025-01-22 23:13:58 -0500

    yay

--- Hosts/laptop/configuration.nix
+++ Hosts/laptop/configuration.nix
@@ -11,6 +11,11 @@
   hardware.bluetooth.enable = true;
   niri.enable = true;
   networking.hostName = "laptop";
+  #boot.kernelParams = [ "amdgpu.dcdebugmask=0x10" ];
+  environment.systemPackages = with pkgs; [
+    python3]
+
+
 
   #iso use only networking.networkmanager.enable = lib.mkForce false;
 
