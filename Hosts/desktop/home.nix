b3fbe9f0051a5858676616f64b0d52018bdddcf6
dhwisema <dhwisema@ncsu.edu>
2025-01-27 15:39:00 -0500

    move home config to single file, change keybinds on laptop niri

--- Hosts/desktop/home.nix
+++ Hosts/desktop/home.nix
@@ -9,8 +9,8 @@
   imports = [
     ./../../modules/default-hm.nix
   ];
-  stylix.enable = true;
 
+ #defining monitors for me niri
   programs.niri.settings.outputs."LG Electronics LG ULTRAGEAR 403MXEZ81564" = {
     position.x = 0;
     position.y = 0;
@@ -21,16 +21,4 @@
     position.y = 0;
   };
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "24.11";
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
