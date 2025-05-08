e6bc6afb11ca0cd26f0ec56dfa0243aafe6e2f5c
dhwisema <dhwisema@ncsu.edu>
2025-05-08 18:11:16 -0400

    delete nixbox as it is a seperate git repo now

--- flake.nix
+++ flake.nix
@@ -103,28 +103,5 @@
         }
       ];
     };
-    nixosConfigurations.Nixbox = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      modules = [
-        
-        ./Hosts/Nixbox/configuration.nix
-
-        home-manager.nixosModules.home-manager
-        {
-          #home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.howard = {pkgs, ...}: {
-            imports = [
-              nur.modules.homeManager.default
-              ./Hosts/Nixbox/home.nix
-            ];
-          };
-
-          # Optionally, use home-manager.extraSpecialArgs to pass
-          # arguments to home.nix
-        }
-      ];
-    };
-
   };
 }
