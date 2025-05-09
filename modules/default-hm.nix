2be984a27e89d85e0546ecfd7aed8471aa234c2f
dhwisema <dhwisema@ncsu.edu>
2025-05-09 02:11:12 -0400

    uh yazi function is actually harder to make work than thought

--- modules/default-hm.nix
+++ modules/default-hm.nix
@@ -38,12 +38,13 @@
   programs.fish.functions = {
     y = {
       body = {"
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
+	set tmp (mktemp -t ""yazi-cwd.XXXXXX"")
+	yazi $argv --cwd-file=""$tmp""
+	if set cwd (command cat -- ""$tmp""); and [ -n ""$cwd"" ]; and [ ""$cwd"" != ""$PWD"" ]
+		builtin cd -- ""$cwd""
 	end
-	rm -f -- "$tmp"
+	rm -f -- ""$tmp""
+  end
       ";};
     };
   };
