7482329862cf50b4b4bc7f743c37d85c258b271e
dhwisema <dhwisema@ncsu.edu>
2025-05-19 19:43:46 -0400

    home manager is now an annoying as fuck project too

--- modules/mako.nix
+++ modules/mako.nix
@@ -13,9 +13,9 @@
       height = 100; # Notification height
       margin = "10"; # Screen margin (e.g., "10" or "20,50")
       padding = "5"; # Inner padding
-      borderSize = 2; # Border thickness
-      borderRadius = 5; # Rounded corners
-      defaultTimeout = 10000; # Timeout in milliseconds (10 seconds)
+      border-size = 2; # Border thickness
+      border-radius = 5; # Rounded corners
+      default-timeout = 10000; # Timeout in milliseconds (10 seconds)
     };
   };
 }
