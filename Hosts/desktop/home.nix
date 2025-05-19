6a35aa70efe78c0bd68282323f14a4a4ac5a29e5
dhwisema <dhwisema@ncsu.edu>
2025-05-19 19:32:46 -0400

    mako change by a refactored module

--- Hosts/desktop/home.nix
+++ Hosts/desktop/home.nix
@@ -6,8 +6,6 @@
 }: {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  home.username = "howard";
-  home.homeDirectory = "/home/howard";
   imports = [
     ./../../modules/default-hm.nix
   ];
--- modules/mako.nix
+++ modules/mako.nix
@@ -4,16 +4,18 @@
   config,
   ...
 }: {
-  services.mako.settings = {
+  services.mako = {
     enable = true;
-    layer = "overlay";
-    anchor = "top-center";
-    width = 300; # Notification width
-    height = 100; # Notification height
-    margin = "10"; # Screen margin (e.g., "10" or "20,50")
-    padding = "5"; # Inner padding
-    borderSize = 2; # Border thickness
-    borderRadius = 5; # Rounded corners
-    defaultTimeout = 10000; # Timeout in milliseconds (10 seconds)
+    settings = {
+      layer = "overlay";
+      anchor = "top-center";
+      width = 300; # Notification width
+      height = 100; # Notification height
+      margin = "10"; # Screen margin (e.g., "10" or "20,50")
+      padding = "5"; # Inner padding
+      borderSize = 2; # Border thickness
+      borderRadius = 5; # Rounded corners
+      defaultTimeout = 10000; # Timeout in milliseconds (10 seconds)
+    };
   };
 }
