dffa29f49b5088734d9c505f7bd5b38227b060a8
dhwisema <dhwisema@ncsu.edu>
2025-05-28 00:49:30 -0400

    add clion, new wallpaper, add home packages instead of it being in default hm

---
+++ modules/packages/home_pkgs.nix
@@ -0,0 +1,14 @@
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    ghostty
+    art
+    libreoffice-fresh
+    yazi
+    kdePackages.konsole
+    distrobox-tui
+    distrobox
+    goofcord
+    appflowy
+    jetbrains.clion
+  ];
+}
