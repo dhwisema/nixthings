35a469c316027bb44c1ba8c68184047955834f0e
dhwisema <dhwisema@ncsu.edu>
2025-06-29 18:11:42 -0400

    ugh

--- Hosts/desktop/home.nix
+++ Hosts/desktop/home.nix
@@ -11,13 +11,13 @@
   ];
 
   #defining monitors for me niri
-  programs.niri.settings.outputs."LG Electronics LG ULTRAGEAR 403MXEZ81564" = {
+  programs.niri.settings.outputs."DP-2" = {
     position.x = 0;
     position.y = 0;
     variable-refresh-rate = false;
     mode = "2560x1440@164.958";
   };
-  programs.niri.settings.outputs."Lenovo Group Limited T24i-2L VKMR6552" = {
+  programs.niri.settings.outputs."DP-1" = {
     position.x = -1920;
     position.y = 0;
   };
