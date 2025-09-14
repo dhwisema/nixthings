bcf31b41dc0d147be704447f42f61eecdfd93d9c
dhwisema <dhwisema@ncsu.edu>
2025-09-14 02:16:36 -0400

    change display outputs due to disabling igpu on 7800x3d

--- modules/desktops/niri/niri-hm.nix
+++ modules/desktops/niri/niri-hm.nix
@@ -7,13 +7,13 @@
 
  
   #defining monitors for me niri
-  programs.niri.settings.outputs."DP-4" = {
+  programs.niri.settings.outputs."DP-1" = {
     position.x = 0;
     position.y = 0;
 #    variable-refresh-rate = false;
     # mode = "2560x1440@164.958";
   };
-  programs.niri.settings.outputs."DP-5" = {
+  programs.niri.settings.outputs."DP-2" = {
     position.x = -1920;
     position.y = 0;
   };
