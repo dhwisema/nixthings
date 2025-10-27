aa736f2222078890226b1daef5c2e63a40cb3653
dhwisema <dhwisema@ncsu.edu>
2025-10-27 13:06:12 -0400

    read in utc

--- modules/common/time.nix
+++ modules/common/time.nix
@@ -1,5 +1,5 @@
 {...}: {
-  time.hardwareClockInLocalTime = true;
+  time.hardwareClockInLocalTime = false;
   time.timeZone = "America/New_York";
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
