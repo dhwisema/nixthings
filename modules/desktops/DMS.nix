d7130962dd14c17937f8af01efd9c2e2f86c7a12
dhwisema <dhwisema@ncsu.edu>
2025-11-08 20:00:01 -0500

    test

--- modules/desktops/DMS.nix
+++ modules/desktops/DMS.nix
@@ -4,10 +4,10 @@
   inputs,
   ...
 }: {
-  imports = [
-    inputs.dankMaterialShell.homeModules.dankMaterialShell.default
-    inputs.dankMaterialShell.homeModules.dankMaterialShell.niri
-  ];
+  # imports = [
+  #   inputs.dankMaterialShell.homeModules.dankMaterialShell.default
+  #   inputs.dankMaterialShell.homeModules.dankMaterialShell.niri
+  # ];
 
   programs.dankMaterialShell = {
     enable = true;
