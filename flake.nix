779efeba08c8d3bba0a220121953353faa066da8
dhwisema <dhwisema@ncsu.edu>
2025-11-21 16:41:51 -0500

    a

--- flake.nix
+++ flake.nix
@@ -52,24 +52,6 @@
       ...
     #waveforms,
     }:
-
-{
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    dgop = {
-      url = "github:AvengeMedia/dgop";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    dankMaterialShell = {
-      url = "github:AvengeMedia/DankMaterialShell";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.dgop.follows = "dgop";
-    };
-  };
-}
-
     {
       nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
