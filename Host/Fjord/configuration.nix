9cc00ee2c6740e3678ef46aa2ec03b281236d649
dhwisema <dhwisema@ncsu.edu>
2026-01-04 03:45:48 -0500

    simplify to just needed bit

--- Host/Fjord/configuration.nix
+++ Host/Fjord/configuration.nix
@@ -3,21 +3,6 @@
   lib,
   ...
 }: {
-  imports = [
-    ./hardware-configuration.nix
-    ./../../modules/default.nix
-  ];
-
-  niri.enable = true;
-  gaming.enable = true;
-  nvidia.enable = true;
-
-
-  
-  # boot.kernelPackages = lib.mkForce pkgs.linuxPackages;
-  networking.hostName = "deskbox";
-  # time.hardwareClockInLocalTime = true;
-
   systemd.services."systemd-suspend" = {
     serviceConfig = {
       Environment = ''"SYSTEMD_SLEEP_FREEZE_USER_SESSIONS=false"'';
