83f24d4a8209adbf79a683f6658140e499b3211a
dhwisema <dhwisema@ncsu.edu>
2026-01-06 00:54:26 -0500

    test

--- flake.nix
+++ flake.nix
@@ -77,7 +77,7 @@
           default-hm = if role == "server" then [ ./Home/server.nix ] else [ ./Home/desktop.nix ];
      
           specialArgs = {
-            inherit hostname;
+            inherit hostname inputs;
           }
           // args;
         in
