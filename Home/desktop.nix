7c37da0b94a3aa4172a10bd312dec0cb80534f1f
dhwisema <dhwisema@ncsu.edu>
2026-01-07 15:50:33 -0500

    i broke something

--- Home/desktop.nix
+++ Home/desktop.nix
@@ -7,59 +7,57 @@
     ./common/programs/home-pkgs-desktop.nix
     ./common/Desktop/niri.nix
   ];
-    stylix.enableReleaseChecks = false;
-    stylix.targets.firefox.profileNames = ["profile_0"];
+  stylix.enableReleaseChecks = false;
+  stylix.targets.firefox.profileNames = [ "profile_0" ];
 
-    options.xdg.userDirs = true;
+  options.xdg.userDirs = true;
 
-   programs = {
-    foot.settings = {
-  main = {
-    term = "xterm-256color";
+  programs.foot.settings = {
+      main = {
+        term = "xterm-256color";
 
-    font = "Fira Code:size=16";
-    dpi-aware = "yes";
-  };
-    }; 
-    firefox = {
-    enable = true;
-
-    profiles = {
-      profile_0 = {
-        # choose a profile name; directory is /home/<user>/.mozilla/firefox/profile_0
-        id = 0; # 0 is the default profile; see also option "isDefault"
-        name = "profile_0"; # name as listed in about:profiles
-        isDefault = true; # can be omitted; true if profile ID is 0
-        userChrome = ''
-
-          #main-window[tabsintitlebar="true"]:not([extradragspace="true"]) #TabsToolbar > .toolbar-items {
-            opacity: 0;
-            pointer-events: none;
-          }
-
-          #main-window:not([tabsintitlebar="true"]) #TabsToolbar {
-              visibility: collapse !important;
-
-          }
-        '';
-        settings = {
-          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-          };
-        # settings = {
-        #   # specify profile-specific preferences here; check about:config for options
-        #   "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
-        #   "browser.startup.homepage" = "https://nixos.org";
-        #   "browser.newtabpage.pinned" = [
-        #     {
-        #       title = "NixOS";
-        #       url = "https://nixos.org";
-        #     }
-        #   ];
-        #   # add preferences for profile_0 here...
-        # };
+        font = "Fira Code:size=16";
+        dpi-aware = "yes";
       };
-  };  
-  
-};
-};
+  };
+  programs.firefox = {
+      enable = true;
+
+      profiles = {
+        profile_0 = {
+          # choose a profile name; directory is /home/<user>/.mozilla/firefox/profile_0
+          id = 0; # 0 is the default profile; see also option "isDefault"
+          name = "profile_0"; # name as listed in about:profiles
+          isDefault = true; # can be omitted; true if profile ID is 0
+          userChrome = ''
+
+            #main-window[tabsintitlebar="true"]:not([extradragspace="true"]) #TabsToolbar > .toolbar-items {
+              opacity: 0;
+              pointer-events: none;
+            }
+
+            #main-window:not([tabsintitlebar="true"]) #TabsToolbar {
+                visibility: collapse !important;
+
+            }
+          '';
+          settings = {
+            "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          };
+          # settings = {
+          #   # specify profile-specific preferences here; check about:config for options
+          #   "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
+          #   "browser.startup.homepage" = "https://nixos.org";
+          #   "browser.newtabpage.pinned" = [
+          #     {
+          #       title = "NixOS";
+          #       url = "https://nixos.org";
+          #     }
+          #   ];
+          #   # add preferences for profile_0 here...
+          # };
+        };
+      };
+
+    };
 }
