75af3e87be64cd7153cf8757f70c74b4f47d917f
dhwisema <112828283+dhwisema@users.noreply.github.com>
2026-01-06 23:32:24 -0500

    Update users.nix

--- Modules/OS/users.nix
+++ Modules/OS/users.nix
@@ -57,7 +57,7 @@
       ];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOM/XkR4NfhrMX2WcGJ10aYnPGcQHg+Jplw5rHowMOo dhwisema@ncsu.edu"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFmXsOd9g4b2fLApvFsqtUOhlSQCxbBX54Cn+RgiB/Sy howard@laptop"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGABYBs0M+mT0nPRRs+Q7Q3O/hnyyHHbcJWVdfuB7T5b irrelevancy@Jester"
       ];
     };
   };
