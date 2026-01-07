eb0db9c8ad8d9bba3c5ac226ebc261b43da84ab6
dhwisema <dhwisema@ncsu.edu>
2026-01-07 02:20:01 -0500

    unfuck

--- Home/desktop.nix
+++ Home/desktop.nix
@@ -6,4 +6,45 @@
     ./common/programs/gaming.nix
     ./common/Desktop/niri.nix
   ];
+
+
+   programs.firefox = {
+    enable = true;
+
+    profiles = {
+      profile_0 = {
+        # choose a profile name; directory is /home/<user>/.mozilla/firefox/profile_0
+        id = 0; # 0 is the default profile; see also option "isDefault"
+        name = "profile_0"; # name as listed in about:profiles
+        isDefault = true; # can be omitted; true if profile ID is 0
+        userChrome = ''
+
+          #main-window[tabsintitlebar="true"]:not([extradragspace="true"]) #TabsToolbar > .toolbar-items {
+            opacity: 0;
+            pointer-events: none;
+          }
+
+          #main-window:not([tabsintitlebar="true"]) #TabsToolbar {
+              visibility: collapse !important;
+
+          }
+        '';
+        settings = {
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          };
+        # settings = {
+        #   # specify profile-specific preferences here; check about:config for options
+        #   "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
+        #   "browser.startup.homepage" = "https://nixos.org";
+        #   "browser.newtabpage.pinned" = [
+        #     {
+        #       title = "NixOS";
+        #       url = "https://nixos.org";
+        #     }
+        #   ];
+        #   # add preferences for profile_0 here...
+        # };
+      };
+  };  
+};
 }
