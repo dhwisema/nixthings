7108fe4423c7f3b2d1e8a0c2fd7093a39c472d18
dhwisema <dhwisema@ncsu.edu>
2026-01-07 15:59:01 -0500

    tst 3

--- Home/desktop.nix
+++ Home/desktop.nix
@@ -9,9 +9,7 @@
   ];
   stylix.enableReleaseChecks = false;
   stylix.targets.firefox.profileNames = [ "profile_0" ];
-config = {
   xdg.userDirs = true;
-};
   programs.foot.settings = {
       main = {
         term = "xterm-256color";
