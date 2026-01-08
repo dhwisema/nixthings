a712d7c637a4129736faa1f00ab5a490fd21dde5
dhwisema <dhwisema@ncsu.edu>
2026-01-07 22:57:37 -0500

    change finger

--- Host/Jester/configuration.nix
+++ Host/Jester/configuration.nix
@@ -5,12 +5,16 @@
 }:
 {
 
-
   services.tlp.enable = pkgs.lib.mkForce false;
 
-  
-
-  services.fprintd.enable = lib.mkDefault true;
+  services.fprintd = {
+    enable = true;
+    package = pkgs.fprintd-tod;
+    tod = {
+      enable = true;
+      driver = pkgs.libfprint-2-tod1-goodix;
+    };
+  };
   boot.kernelParams = [ "amdgpu.dcdebugmask=0x10" ]; # disable psr-su
 
   #iso use only networking.networkmanager.enable = lib.mkForce false;
