58e20c6174bfb525fd9982df2bac6590f237f56f
dhwisema <dhwisema@ncsu.edu>
2026-01-12 22:00:40 -0500

    CWA

---
+++ Modules/Containers/CWA.nix
@@ -0,0 +1,26 @@
+{nixpkgs,hostname,lib,...}:{
+  virtualisation.oci-containers.Calibre-Web-Automate = {
+    pull = "newer";
+    image = "crocodilestick/calibre-web-automated:latest";
+    
+    
+    environment = {
+ DATABASE_HOST = "db.example.com";
+  DATABASE_PORT = "3306";
+  PUID = 1000;
+  PGID = 1000;
+  TZ = "EDT";
+  HARDCOVER_TOKEN="";
+  CWA_PORT_OVERRIDE=8083;
+  NETWORK_SHARE_MODE=false;
+  
+  
+};
+    volumes = [
+      "/home/irrelevancy/CWA/config:/config"
+      "/home/irrelevancy/CWA/ingest:/cwa-book-ingest"
+      "/home/irrelevancy/CWA/library:/calibre-library"
+    ];
+    ports =[ "8083:8083" ];
+  };
+}
