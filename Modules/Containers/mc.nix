f9440f03e3e98ceb9ce9696a49f0a4a7e86479f6
dhwisema <dhwisema@ncsu.edu>
2026-01-26 01:26:40 -0500

    fix dependecny and also re add things cause they werent the issue it seems

--- Modules/Containers/mc.nix
+++ Modules/Containers/mc.nix
@@ -26,12 +26,22 @@
           view-distance-fix
           towns-and-towers
           noisiumforked
-          fabric-api
-        '';
+          fabric-apii
+          https://modrinth.com/plugin/geyser
+          https://modrinth.com/datapack/terralith
+          https://modrinth.com/datapack/structory
+          https://modrinth.com/datapack/incendium
+          https://modrinth.com/datapack/structory-towers
+          https://modrinth.com/datapack/remove-terralith-intro-message
+          https://modrinth.com/plugin/lmd
+           https://modrinth.com/datapack/dungeons-and-taverns
+          '';
         VERSION = "1.21.11";
         MEMORY = "16384M";
         USE_MEOWICE_FLAGS = "true";
         TZ = "America/New_York";
+        MODRINTH_DOWNLOAD_DEPENDENCIES = "optional";
+        MODRINTH_ALLOWED_VERSION_TYPE =  "beta";
       };
        extraOptions = [
         "--interactive"
