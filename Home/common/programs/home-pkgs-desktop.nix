887f6e3024add294c1f3f9875872efb1c0ea58f1
dhwisema <dhwisema@ncsu.edu>
2026-01-27 16:21:26 -0500

    add qtcreator

--- Home/common/programs/home-pkgs-desktop.nix
+++ Home/common/programs/home-pkgs-desktop.nix
@@ -5,6 +5,7 @@
     onlyoffice-desktopeditors
     # distrobox-tui
     # distrobox #dont need this currently will reneable if i have to use code composer again.
+    qtcreator
     uv
     python314
     vesktop
