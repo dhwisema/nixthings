09c00e17d65b97110a4f03c3e0e05e3185b34103
dhwisema <dhwisema@ncsu.edu>
2026-01-28 14:20:49 -0500

    add support to run as non root?

--- Modules/Programs/wireshark.nix
+++ Modules/Programs/wireshark.nix
@@ -2,4 +2,11 @@
   programs.wireshark.enable=true;
   programs.wireshark.package = pkgs.wireshark;
   users.users.${username}.extraGroups = ["wireshark"];
+
+    services.udev = {
+    extraRules = ''
+      SUBSYSTEM=="usbmon", GROUP="wireshark", MODE="0640"
+    '';
+  };
+
 }
