e5a5ace696dd38971fd72d3ed01c8a47b5249b5b
dhwisema <dhwisema@ncsu.edu>
2026-01-28 14:14:39 -0500

    typo in prgroams

--- Modules/Programs/wireshark.nix
+++ Modules/Programs/wireshark.nix
@@ -1,5 +1,5 @@
 {pkgs,config,lib,username,...}:{
-  prgroams.wireshark.enable=true;
+  programs.wireshark.enable=true;
   programs.wireshark.package = pkgs.wireshark;
   users.users.${username}.extraGroups = ["wireshark"];
 }
