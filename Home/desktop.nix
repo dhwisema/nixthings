d39fc5bfa2232bd913ab39bbe644c1496dd9c9d8
dhwisema <dhwisema@ncsu.edu>
2026-01-28 15:55:34 -0500

    set theme to stylix by default

--- Home/desktop.nix
+++ Home/desktop.nix
@@ -21,49 +21,59 @@
     enable = true;
     systemd.enable = true;
     systemd.autoStart = true;
+    settings = {
+      theme = {
+        light = {
+          name = "stylix";
+        };
+        dark = {
+          name = "stylix";
+        };
+      };
+    };
   };
   programs.fish.enable = true;
   programs.zoxide.enable = true;
   programs.zoxide.enableFishIntegration = true;
   programs.helix.enable = true;
   programs.firefox = {
-      enable = true;
+    enable = true;
 
-      profiles = {
-        profile_0 = {
-          # choose a profile name; directory is /home/<user>/.mozilla/firefox/profile_0
-          id = 0; # 0 is the default profile; see also option "isDefault"
-          name = "profile_0"; # name as listed in about:profiles
-          isDefault = true; # can be omitted; true if profile ID is 0
-          userChrome = ''
+    profiles = {
+      profile_0 = {
+        # choose a profile name; directory is /home/<user>/.mozilla/firefox/profile_0
+        id = 0; # 0 is the default profile; see also option "isDefault"
+        name = "profile_0"; # name as listed in about:profiles
+        isDefault = true; # can be omitted; true if profile ID is 0
+        userChrome = ''
 
-            #main-window[tabsintitlebar="true"]:not([extradragspace="true"]) #TabsToolbar > .toolbar-items {
-              opacity: 0;
-              pointer-events: none;
-            }
+          #main-window[tabsintitlebar="true"]:not([extradragspace="true"]) #TabsToolbar > .toolbar-items {
+            opacity: 0;
+            pointer-events: none;
+          }
 
-            #main-window:not([tabsintitlebar="true"]) #TabsToolbar {
-                visibility: collapse !important;
+          #main-window:not([tabsintitlebar="true"]) #TabsToolbar {
+              visibility: collapse !important;
 
-            }
-          '';
-          settings = {
-            "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-          };
-          # settings = {
-          #   # specify profile-specific preferences here; check about:config for options
-          #   "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
-          #   "browser.startup.homepage" = "https://nixos.org";
-          #   "browser.newtabpage.pinned" = [
-          #     {
-          #       title = "NixOS";
-          #       url = "https://nixos.org";
-          #     }
-          #   ];
-          #   # add preferences for profile_0 here...
-          # };
+          }
+        '';
+        settings = {
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         };
+        # settings = {
+        #   # specify profile-specific preferences here; check about:config for options
+        #   "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
+        #   "browser.startup.homepage" = "https://nixos.org";
+        #   "browser.newtabpage.pinned" = [
+        #     {
+        #       title = "NixOS";
+        #       url = "https://nixos.org";
+        #     }
+        #   ];
+        #   # add preferences for profile_0 here...
+        # };
       };
-
     };
+
+  };
 }
